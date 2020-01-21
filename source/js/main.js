4b4e38ddc0df9f8b66bd61d1a91b61f751e7da01
kovarnyirus <kovarnyi@gmail.com>
2020-01-22 01:20:08 +0300

    add dog img

--- source/js/main.js
+++ source/js/main.js
@@ -190,4 +190,16 @@ $( document ).ready(function() {
     preloader: false,
   });
 
+  $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
+
 });
